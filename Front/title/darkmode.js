8e13b8f0793002e55fc6251a46a3ebdf74327b19
황교진 <84490050+KyoJin-Hwang@users.noreply.github.com>
2022-07-14 11:04:19 +0900

    title bgcolor

--- Front/title/darkmode.js
+++ Front/title/darkmode.js
@@ -1,13 +1,14 @@
+// 시간대별 background-color 변경
 const date = new Date();
 const hour = date.getHours();// 0~23시간 시간 정수 값 얻기
 if (7 <= hour && hour <= 18){  // 오전7 이상 오후 6시 미만  
-    document.body.classList.add('light')
-}else{
     document.body.classList.add('dark')
+}else{
+    document.body.classList.add('light')
 }
 console.log(hour)
 
-// change dark
+// 클릭모션으로 다크모드 이벤트
 const checkbox = 
 document.getElementById('checkbox');
 checkbox.addEventListener('click', () => {
