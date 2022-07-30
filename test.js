0e61f3c6ce3e80bc6b6831573d2815529d8a7598
황교진 <84490050+KyoJin-Hwang@users.noreply.github.com>
2022-07-31 00:12:01 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,23 +1,25 @@
-$(document).ready(() => {
+var songTitle = localStorage.getItem('mtitle')
+var songArtist = localStorage.getItem('martist')
+console.log(songTitle)
+console.log(songArtist)
+let allMusic = [
+   {
+       for(let i = 0; i<=songTitle; i++){
+       
+       name : songTitle[i],
+       artist : songArtist[i],
+       img : "music-1",
+       audio : songTitle[i]
+       }
+   },
+   {
+       name : "나의 X에게",
+       artist : "경서",
+       img : "music-3",
+       audio : "music-3"
+   },
+]
 
-	// ajax통신, 글데이터 가져오기
-	$.ajax({
-		url: 'MusicList.do', // 어디로 요청을 보낼 것인가? 
-		type: 'post', //get?, post?
-		data: {
-		'key(name)' : value  // -> 보내줄데이터
-		},
-		/* dataType : 'json',  응답 받는 데이터가 어떤 형태인지 */
-		success: function (res) {
-			// 요청이 성공했을때  실행되는 Call back 함수 
-			// res --> 응답받는 데이터가 담기는 변수
-			console.log(res);
-			},
-		error: function (e) {
-			// 404,405,500 --> 요청자체가 실패
-			// e--> 어떤 에러인지 담기는 변수
-			alert('error');
-		}
-	})
-})
+
+
 
